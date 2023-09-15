29fe2728f128adf911b8a1daa2328c6d1f9f9a76
Thirudinesh <46817057+thirudinesh71@users.noreply.github.com>
2023-09-15 16:12:16 +0530

    Add files via upload

---
+++ psgApplication/psgApplication/src/test/java/com/example/psgApplication/PsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.psgApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
